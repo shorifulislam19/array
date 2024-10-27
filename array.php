95e4b710290ac593c9ae5a66d5e3e86b0900c942
Shoriful Islam Shakil <shoriful15-2485@diu.edu.bd>
2024-10-27 11:41:40 +0600

    first commit

---
+++ array.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Array</title>
+  </head>
+  <body>
+    <h3 class="text-center mt-5">Practice Array</h3>
+
+
+    
+
+
+    <?php 
+        $person_name = "Shoriful";
+        $person_email = "shoriful@gmail.com";
+        $person_number = "01791379767";
+
+        echo  '$person_name' ."</br>";
+        echo  '$person_email'."</br>";
+        echo  '$person_number';
+
+
+        ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
